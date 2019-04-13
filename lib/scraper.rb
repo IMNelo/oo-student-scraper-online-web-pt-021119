a5ec5936103d44421d21777fc589e8b5efc63eb7
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-13 20:42:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url) #responsible for scraping the index page that lists all of the students
+<<<<<<< HEAD
     doc = Nokogiri::HTML(open ("./fixtures/student-site/index.html"))
 
     students = []
@@ -29,6 +30,29 @@ class Scraper
 
     #binding.pry
     end
+=======
+    #doc = Nokogiri::HTML(open(./fixtures/student-site/index.html)) #insert the URL of the web site we're scraping
+    #student_card = doc.css(".student-card")
+    #The return value of this method should be an array of hashes in which each hash represents a single student.
+    #Array of hashes in which each hash represents a single students = [{student1, student2, student3]
+    #students = []
+      # student_card.each do |card|
+      #   students << {
+      #   :name => card.css(),
+      #   :location => ,
+      #   :profile_url =>
+      # }
+      # end
+
+      students #returns an array of hashes in which each hash represents a single student.
+    end
+    #binding.pry
+
+
+  def self.scrape_profile_page(profile_url) #responsible for scraping an individual student's profile page to get further information about that student
+    #doc = Nokogiri::HTML(open(profile_url))
+    #doc.css()
+>>>>>>> 8158a1290f67054121bcc8af2a1377be9c0c5689
   end
 
 end
