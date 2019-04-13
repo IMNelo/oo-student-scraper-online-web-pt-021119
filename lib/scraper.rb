daeb5b3a035f7a025562410b9eb91fdaa924ad23
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-13 07:41:39 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,34 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) #responsible for scraping the index page that lists all of the students
+    doc = Nokogiri::HTML(open ("./fixtures/student-site/index.html"))
+
+    students = []
+
+    doc.css("div.student-card").each do |student|
+      students << student = {
+      #name: doc.css("div.card-text-container h4.student-name").first.text,
+      #location: doc.css("div.card-text-container p.student-location").first.text,
+      #profile_url: doc.css("div.student-card a").first.text  # ***Grab the link this one ***
+      }
+        #binding.pry
+    end
   end
 
-  def self.scrape_profile_page(profile_url)
-    
+
+
+  def self.scrape_profile_page(profile_url) #responsible for scraping an individual student's profile page to get further information about that student
+    doc = Nokogiri::HTML(open (profile_url))
+    students = {}
+    doc.css("div.social-icon-container a").each do |social_link|
+      link = social_link.attribute("href").text
+
+    #binding.pry
+    end
   end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,26 @@
+require_relative 'scraper.rb'
+
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+  #uses the Scraper class to create new students w/the correct name & location.
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+  #uses the Scraper class to get a hash of a given students attributes & uses that hash to set additional attributes for that student.
+
   end
 
   def self.all
-    
+    @@all
   end
 end
-
